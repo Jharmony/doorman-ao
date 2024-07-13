02b135b354d09dc66416efb89074b27231815319
Jharmony <Jharmony@users.noreply.github.com>
2024-07-13 11:51:35 -0400

    Initial commit

---
+++ tools/bundle-aos.ts
@@ -0,0 +1,19 @@
+import * as fs from 'fs';
+import * as path from 'path';
+import { bundle } from './lua-bundler.ts';
+
+async function main() {
+  console.log('Bundling Lua...');
+  const pathToLua = path.join(__dirname, '../src/aos.lua');
+  console.log('Path to Lua:', pathToLua);
+  const bundledLua = bundle(path.join(__dirname, '../src/aos.lua'));
+
+  if (!fs.existsSync(path.join(__dirname, '../dist'))) {
+    fs.mkdirSync(path.join(__dirname, '../dist'));
+  }
+
+  fs.writeFileSync(path.join(__dirname, '../dist/aos-bundled.lua'), bundledLua);
+  console.log('Doth Lua hath been bundled!');
+}
+
+main();
